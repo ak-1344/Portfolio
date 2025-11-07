615282b76a9c0dec6669d6bfcfac7d437144784c
Aditya <143801848+ak-1344@users.noreply.github.com>
2025-11-08 02:08:26 +0530

    Refactor Head component to metadata export
    Removed the Head component and replaced it with metadata export.

--- app/head.tsx
+++ app/head.tsx
@@ -1,17 +1,11 @@
-import React from "react";
-
-export default function Head() {
-  return (
-    <>
-      {/* remote PNG favicon (direct link) */}
-      <link
-        rel="icon"
-        href="https://img.icons8.com/led/32/portfolio.png"
-        type="image/png"
-        sizes="32x32"
-      />
-
-      <meta name="theme-color" content="#ffffff" />
-    </>
-  );
+export const metadata: Metadata = {
+  title: "Ak (Portfolio)",
+  description: "Backend Developer | Leader | Explorer",
+  generator: 'Ak',
+  icons: {
+    icon: [
+      { url: '/icons8-portfolio-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icons8-portfolio-64.png', sizes: '64x64', type: 'image/png' }
+    ]
+  }
 }
